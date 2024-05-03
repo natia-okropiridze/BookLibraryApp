96573ff8f5eb5170dae6820c4fbe42b015ee7d73
natia-okropiridze <natiaokropiridze0@gmail.com>
2024-05-03 18:07:22 +0400

    Filter Redux slice and filter by title

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,18 +1,28 @@
 import { useSelector, useDispatch } from "react-redux";
 import { deleteBook, toggleFavorite } from "../../redux/books/actionCreators";
 import { BsBookmarkStarFill, BsBookmarkStar } from "react-icons/bs";
+import { selectTitleFilter } from "../../redux/slices/filterSlice";
 import "./BookList.css";
 const BookList = () => {
   const books = useSelector((state) => state.books);
+  const titleFilter = useSelector(selectTitleFilter);
   const dispatch = useDispatch();
   const handleDeleteBook = (id) => {
     if (window.confirm("Are You Sure  to Delete?")) {
       dispatch(deleteBook(id));
     }
   };
+
   const handleToggleFavorite = (id) => {
     dispatch(toggleFavorite(id));
   };
+
+  const filteredBooks = books.filter((book) => {
+    const matchesTitle = book.title
+      .toLowerCase()
+      .includes(titleFilter.toLowerCase());
+    return matchesTitle;
+  });
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
@@ -20,7 +30,7 @@ const BookList = () => {
         <p>No Books available</p>
       ) : (
         <ul>
-          {books.map((book, index) => (
+          {filteredBooks.map((book, index) => (
             <li key={book.id}>
               <div className="book-info">
                 {++index}. {book.title} by <strong>{book.author}</strong>
